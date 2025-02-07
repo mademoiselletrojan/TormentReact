30bf650167038c51e7332e1de960a313ff842994
Trojan <bonnie.alina2016@yandex.ru>
2025-02-07 11:51:46 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function App() {
+  let likes = 5;
+
+  function increment() {
+    likes += 1;
+    console.log(likes);
+  }
+  function decrement() {
+    likes -= 1;
+    console.log(likes);
+  }
+
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
