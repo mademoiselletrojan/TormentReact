135264a4204238065bab929fc163786fb503989b
Trojan <bonnie.alina2016@yandex.ru>
2025-02-12 16:43:09 +0300

    Added the massive const, used the method map

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import './styles/App.scss';
 import PostItem from "./components/PostItem";
 
 function App() {
-  const [value, setValue] = useState("text");
+  const[posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Typescript', body: 'Description'},
+    {id: 3, title: 'React', body: 'Description'}
+  ])
 
   return (
     <div className="App">
-      <PostItem post ={{id: 1, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post ={{id: 2, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post ={{id: 3, title: 'Javascript', body: 'Description'}}/>
+      <h1 style ={{textAlign: 'center'}}>Список постов</h1>
+      {posts.map(post => <PostItem post={post} key ={post.id}/>
+
+      )}
     </div>
   );
 }
