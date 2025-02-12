a88f41ad08836d406f246385e6351b925395aeeb
Trojan <bonnie.alina2016@yandex.ru>
2025-02-12 10:10:57 +0300

    event onChange

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
 
 function App() {
-  const [likes, setLikes] = useState(5);
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-  function decrement() {
-    setLikes(likes - 1);
-  }
+  const [value, setValue] = useState("text");
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
     </div>
   );
 }
